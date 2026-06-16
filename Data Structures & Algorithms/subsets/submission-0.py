c015b248a8bdc595765c13a5899e2a094a2d4e2c
praneemamidi04042005 <praneeth.23bce9683@vitapstudent.ac.in>
2026-06-16 16:48:51 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ansList=[]
+        ds=[]
+        def findSubsets(ind,nums):
+            ansList.append(ds[:])
+            for i in range(ind,len(nums)):
+                if i!=ind and nums[i]==nums[i-1]:
+                    continue
+                ds.append(nums[i])
+                findSubsets(i+1,nums)
+                ds.remove(ds[len(ds)-1])
+        nums.sort()
+        findSubsets(0,nums)
+        return ansList
